0d0a8c4bcc2cc302411b0bf1d529e22340b0ef60
KimJaedeok <jdcoder27@gmail.com>
2021-05-16 22:24:48 +0900

    ADD: chatting

--- src/loaders/socket/index.js
+++ src/loaders/socket/index.js
@@ -5,13 +5,20 @@ const { Server } = require("socket.io");
 const io = new Server(server, {
   cors: {
     origin: "*",
-  }
+  },
 });
 
 io.on("connection", (socket) => {
-  console.log("a user connected");
-  socket.on("disconnect", () => {
-    console.log("user disconnected");
+  socket.on("join streaming", (id) => {
+    socket.join(id);
+
+    socket.on("chat", (chat) => {
+      socket.broadcast.to(id).emit("other user chat", chat);
+    });
+  });
+
+  socket.on("leave streaming", (id) => {
+    socket.leave(id);
   });
 });
 
